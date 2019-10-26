c8e05aaab0b2442ad3804a48d6c9fe7dd794e63b
Heejin Ohn <heejin-ohn@ou.edu>
2019-10-25 19:49:46 -0500

    Update atsim.py file

--- 1_code/atsim.py
+++ 1_code/atsim.py
@@ -3,12 +3,17 @@ atsim.py
 """
 import pandas as pd, numpy as np, os
 directory = '/scratch/ou/hohn/TNIC_AllPairsDistrib/'
-atsim = pd.read_csv('/scratch/ou/hohn/upload.csv', index_col=['gvkey1', 'gvkey2', 'year'])
+atsim = pd.read_csv('/scratch/ou/hohn/upload.csv', index_col=['gvkey1', 'gvkey2', 'year'],
+                    dtype={'gvkey1':'Int64', 'gvkey2':'Int64', 'year':'Int64', 'score':'float64'}, na_values='NaN')    
 
 for filename in os.listdir(directory):
     if filename.endswith('.txt'):
-        tnic = pd.read_csv(os.path.join(directory, filename), delimiter='\t', header=0, index_col=['gvkey1', 'gvkey2', 'year'])
-        atsim = atsim.merge(tnic, how='left', left_index=True, right_index=True)
+        tnic = pd.read_csv(os.path.join(directory, filename), 
+                           delimiter='\t', usecols=['gvkey1','gvkey2','year','score'],
+                           dtype={'gvkey1':'Int16', 'gvkey2':'Int16', 'year':'Int16', 'score':'float64'}, na_values='NaN')    
+        tnic.drop_duplicates(subset=['gvkey1', 'gvkey2', 'year'], inplace=True)
+        tnic.set_index(['gvkey1', 'gvkey2', 'year'], inplace=True)
+        atsim.update(tnic, join='left')
         del tnic
-    
+
 atsim.to_csv('/scratch/ou/hohn/atsim.csv')
