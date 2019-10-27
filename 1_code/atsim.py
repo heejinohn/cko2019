b2b2b8a1c679b8068bcb296ff77b7ec50e98b605
Heejin Ohn <heejin-ohn@ou.edu>
2019-10-27 18:12:36 -0500

    Update Review_IV file

--- 1_code/atsim.py
+++ 1_code/atsim.py
@@ -5,7 +5,7 @@ import pandas as pd, numpy as np, os
 directory = '/scratch/ou/hohn/TNIC_AllPairsDistrib/'
 atsim = pd.read_csv('/scratch/ou/hohn/upload.csv', index_col=['gvkey1', 'gvkey2', 'year'],
                     dtype={'gvkey1':'Int64', 'gvkey2':'Int64', 'year':'Int64', 'score':'float64'}, na_values='NaN')    
-
+atsim['score'] = np.NaN
 for filename in os.listdir(directory):
     if filename.endswith('.txt'):
         tnic = pd.read_csv(os.path.join(directory, filename), 
