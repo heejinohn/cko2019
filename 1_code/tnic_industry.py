c86f78d1411cbb3786be8a7d4a580f71efae6368
Heejin Ohn <heejin-ohn@ou.edu>
2019-10-30 18:58:59 -0500

    Fix Pipfile to include neovim

--- 1_code/tnic_industry.py
+++ 1_code/tnic_industry.py
@@ -1,20 +1,20 @@
 """
 tnic_industry.py
 """
-import pandas as pd
 import numpy as np, os
-directory = '/scratch/ou/hohn/TNIC_AllPairsDistrib/'
-tnic_industry = pd.read_pickle('~/tnic_industry.pkl')
-for filename in os.listdir(directory):
+import pandas as pd
+DIRECTORY = '/scratch/ou/hohn/TNIC_AllPairsDistrib/'
+TNIC_INDUSTRY = pd.read_pickle('~/tnic_industry.pkl')
+for filename in os.listdir(DIRECTORY):
     if filename.endswith('.pkl'):
-        tnic = pd.read_pickle(os.path.join(directory, filename), compression='gzip')
+        tnic = pd.read_pickle(os.path.join(DIRECTORY, filename), compression='gzip')
         for i in range(1,3):
-            tnic_industry.rename(columns={'score' + '_' + str(i):'score'}, inplace=True)
-            tnic_industry.index = tnic_industry.index.set_levels(tnic_industry.index.levels[1] + 1, level=1)
-            tnic_industry.update(tnic, join='left', overwrite=False)
-            tnic_industry.rename(columns={'score':'score' + '_' + str(i)}, inplace=True)
-        tnic_industry.index = tnic_industry.index.set_levels(tnic_industry.index.levels[1] - 2, level=1)
+            TNIC_INDUSTRY.rename(columns={'score' + '_' + str(i):'score'}, inplace=True)
+            TNIC_INDUSTRY.index = TNIC_INDUSTRY.index.set_levels(TNIC_INDUSTRY.index.levels[1] + 1, level=1)
+            TNIC_INDUSTRY.update(tnic, join='left', overwrite=False)
+            TNIC_INDUSTRY.rename(columns={'score':'score' + '_' + str(i)}, inplace=True)
+        TNIC_INDUSTRY.index = TNIC_INDUSTRY.index.set_levels(TNIC_INDUSTRY.index.levels[1] - 2, level=1)
         del tnic
 
 
-tnic_industry.to_pickle('/scratch/ou/hohn/tnic_ind_update.pkl')
+TNIC_INDUSTRY.to_pickle('/scratch/ou/hohn/tnic_ind_update.pkl')
