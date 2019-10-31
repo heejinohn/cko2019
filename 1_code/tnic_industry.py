75cabbafb34527edd245fbc998dbc8a3fd752416
Heejin Ohn <heejin-ohn@ou.edu>
2019-10-30 20:19:01 -0500

    Update Review_IV file

--- 1_code/tnic_industry.py
+++ 1_code/tnic_industry.py
@@ -1,19 +1,25 @@
 """
 tnic_industry.py
 """
-import numpy as np, os
+import numpy as np
+import os
 import pandas as pd
 DIRECTORY = '/scratch/ou/hohn/TNIC_AllPairsDistrib/'
 TNIC_INDUSTRY = pd.read_pickle('~/tnic_industry.pkl')
 for filename in os.listdir(DIRECTORY):
     if filename.endswith('.pkl'):
-        tnic = pd.read_pickle(os.path.join(DIRECTORY, filename), compression='gzip')
-        for i in range(1,3):
-            TNIC_INDUSTRY.rename(columns={'score' + '_' + str(i):'score'}, inplace=True)
-            TNIC_INDUSTRY.index = TNIC_INDUSTRY.index.set_levels(TNIC_INDUSTRY.index.levels[1] + 1, level=1)
+        tnic = pd.read_pickle(os.path.join(
+            DIRECTORY, filename), compression='gzip')
+        for i in range(1, 3):
+            TNIC_INDUSTRY.rename(
+                columns={'score' + '_' + str(i): 'score'}, inplace=True)
+            TNIC_INDUSTRY.index = TNIC_INDUSTRY.index.set_levels(
+                TNIC_INDUSTRY.index.levels[1] + 1, level=1)
             TNIC_INDUSTRY.update(tnic, join='left', overwrite=False)
-            TNIC_INDUSTRY.rename(columns={'score':'score' + '_' + str(i)}, inplace=True)
-        TNIC_INDUSTRY.index = TNIC_INDUSTRY.index.set_levels(TNIC_INDUSTRY.index.levels[1] - 2, level=1)
+            TNIC_INDUSTRY.rename(
+                columns={'score': 'score' + '_' + str(i)}, inplace=True)
+        TNIC_INDUSTRY.index = TNIC_INDUSTRY.index.set_levels(
+            TNIC_INDUSTRY.index.levels[1] - 2, level=1)
         del tnic
 
 
